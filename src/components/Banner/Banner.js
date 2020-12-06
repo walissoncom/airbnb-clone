126d451e6a2123144f5e5848cfbb6188699f65a9
Walisson Rodrigues <contact@walisson.com>
2020-12-06 15:10:40 +1100

    Search component UI implementation + Importing react-date-range

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import { Button } from '@material-ui/core';
 
+import Search from '../Search/Search';
+
 import './Banner.css';
 
 function Banner() {
@@ -10,6 +12,10 @@ function Banner() {
 
     return (
         <div className="banner">
+            <div className="banner__search">
+                <Button onClick={() => setShowSearch(!showSearch)} className="banner__searchButton" variant="outlined">{showSearch ? 'Hide' : 'Search Dates'}</Button>
+                {showSearch && <Search />}
+            </div>
             <div className="banner__info">
                 <h1>Get out and stretch your imagination</h1>
                 <h5>Plan a different kind of getaway to uncover the hidden gems near you.</h5>
