cac1c9d8356721acbfd37f772bc34d79eb6f5748
Walisson Rodrigues <contact@walisson.com>
2020-12-06 13:59:06 +1100

    Home component - Banner component UI implementation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Home from './components/Home/Home';
 import Header from './components/Header/Header';
 
 import './App.css';
@@ -6,6 +7,8 @@ function App() {
   return (
     <div className="app">
       <Header />
+
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Banner from '../Banner/Banner';
+
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <Banner />
+        </div>
+    )
+}
+
+export default Home;
