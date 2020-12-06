01e4a3d218aaea6b7b0de0b937c60a1baf770d19
Walisson Rodrigues <contact@walisson.com>
2020-12-06 13:00:39 +1100

    Header component UI implementation + Material UI import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Header from './components/Header/Header';
+
+import './App.css';
+
 function App() {
   return (
     <div className="app">
-      <h1>Airbnb Clone</h1>
+      <Header />
     </div>
   );
 }
