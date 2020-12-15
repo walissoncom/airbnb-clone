2cf24e2679ba94e5c43674eadb66fb2fdf973fd6
Walisson Rodrigues <contact@walisson.com>
2020-12-15 11:24:43 +1100

    Search Page content

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -28,6 +28,60 @@ function SearchPage() {
                 price="$80 / night"
                 total="$217 total" />
 
+            <SearchResult
+                img="https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg"
+                location="Private room in center of Sydney"
+                title="Independant luxury studio apartment"
+                description="2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen"
+                star={4.3}
+                price="$120 / night"
+                total="$357 total" />
+
+            <SearchResult
+                img="https://www.smartertravel.com/uploads/2017/07/Untitled-design-8.jpg"
+                location="Private room in center of Sydney"
+                title="Sydney Studio Apartments"
+                description="4 guest · 4 bedroom · 4 bed · 2 bathrooms · Free parking · Washing Machine"
+                star={3.8}
+                price="$95 / night"
+                total="$307 total" />
+
+            <SearchResult
+                img="https://cdn.bisnow.net/fit?height=489&type=jpeg&url=https%3A%2F%2Fs3.amazonaws.com%2Fcdn.bisnow.net%2Fcontent%2Fimages%2F2017%2F05%2F59151d0978bbf_https_press_atairbnb_com_app_uploads_2016_12_midtown_4.jpeg&width=717&sign=FeltIPi9cOWA36nVIeDvZxwgtiCZrpUyMRdvyZviTUI"
+                location="Private room in center of Sydney"
+                title="30 mins to Oxford Street, Excel Sydney"
+                description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+                star={4.1}
+                price="$85 / night"
+                total="$300 total" />
+
+            <SearchResult
+                img="https://media.cntraveler.com/photos/5a8f258bd363c34048b35aac/master/w_2250,h_1500,c_limit/airbnb-plus-london.jpg"
+                location="Private room in center of Sydney"
+                title="Spacious Peaceful Modern Bedroom"
+                description="3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Free parking · Dry Cleaning"
+                star={5.0}
+                price="$108 / night"
+                total="$450 total" />
+
+            <SearchResult
+                img="https://static.trip101.com/paragraph_media/pictures/001/676/061/large/969ae4bb-efd1-4fb9-a4e3-5cb3316dd3c9.jpg?1562227937"
+                location="Private room in center of Sydney"
+                title="The Blue Room In Sydney"
+                description="2 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Washing Machine"
+                star={4.23}
+                price="$68 / night"
+                total="$180 total" />
+
+            <SearchResult
+                img="https://image.insider.com/585029a0dd0895bc548b4b8b?width=750&format=jpeg&auto=webp"
+                location="Private room in center of Sydney"
+                title="5 Star Luxury Apartment"
+                description="3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+                star={3.85}
+                price="$190 / night"
+                total="$750 total" />
+
         </div>
     )
 }
