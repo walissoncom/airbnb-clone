9dc56e87b44c672fc732bab0dc0207418929d323
Walisson Rodrigues <contact@walisson.com>
2020-12-15 11:12:48 +1100

    Search Result component UI implementation

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Button } from '@material-ui/core';
 
+import SearchResult from '../../components/SearchResult/SearchResult';
+
 import './SearchPage.css';
 
 function SearchPage() {
@@ -16,6 +18,16 @@ function SearchPage() {
                 <Button variant="outlined">Rooms and beds</Button>
                 <Button variant="outlined">More filters</Button>
             </div>
+
+            <SearchResult
+                img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
+                location="Private room in center of Sydney"
+                title="Stay at this spacious City Apartment"
+                description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+                star={4.73}
+                price="$80 / night"
+                total="$217 total" />
+
         </div>
     )
 }
